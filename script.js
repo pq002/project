083d2ccdfde56e815ddb1209b416609702ef391b
pq002 <64705160+pq002@users.noreply.github.com>
2021-01-12 17:16:54 -0500

    added a lot more stuff

--- script.js
+++ script.js
@@ -1,2 +1,11 @@
 let words = ['fries', 'chicken nuggets', 'handburger', 'hot dog', 'icecream', 'fried chicken', 'donuts', 'cup cake', 'cake', 'pan cake', 'sauce', 'apple pie', 'macaroni', 'pasta', 
-'nachos', 'pizz'];
+'nachos', 'pizza'];
+let size = words.length;
+let randomNum = Math.Floor(Math.random() * size);
+let word = words[randomNum];
+
+for (let i = 0; i < word; i++) {
+    if (word == ' ') {
+        
+    }
+}
