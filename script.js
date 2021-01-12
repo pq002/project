3601219482d74f28412429311394b93c5bc4d52b
pq002 <64705160+pq002@users.noreply.github.com>
2021-01-12 16:40:27 -0500

    added button and input colored the backround and a list

--- script.js
+++ script.js
@@ -1,1 +1,2 @@
-console.log('test')
+let words = ['fries', 'chicken nuggets', 'handburger', 'hot dog', 'icecream', 'fried chicken', 'donuts', 'cup cake', 'cake', 'pan cake', 'sauce', 'apple pie', 'macaroni', 'pasta', 
+'nachos', 'pizz'];
