ec96329dd21d202a7ecd27724f9972b0d3eaa425
pq002 <64705160+pq002@users.noreply.github.com>
2021-01-19 16:49:42 -0500

    fixed errors

--- script.js
+++ script.js
@@ -1,11 +1,13 @@
-let words = ['fries', 'chicken nuggets', 'handburger', 'hot dog', 'icecream', 'fried chicken', 'donuts', 'cup cake', 'cake', 'pan cake', 'sauce', 'apple pie', 'macaroni', 'pasta', 
+const words = ['fries', 'chicken nuggets', 'handburger', 'hot dog', 'icecream', 'fried chicken', 'donuts', 'cup cake', 'cake', 'pan cake', 'sauce', 'apple pie', 'macaroni', 'pasta', 
 'nachos', 'pizza'];
 let size = words.length;
-let randomNum = Math.Floor(Math.random() * size);
-let word = words[randomNum];
+let randomNum = Math.floor(Math.random() * size);
+let Word = words[randomNum];
 
-for (let i = 0; i < word; i++) {
-    if (word == ' ') {
-        
-    }
+for (let i = 0; i < words[i].length; i++) {
+  word.innerHTML += '_ ';
 }
+
+guessButton.addEventListener('load', () => {
+    
+  });
